19a3c355e81b2b6bceb8aba39b589e42e8bc1c52
hiroshagure <hiroshagure@gmail.com>
2019-11-28 15:36:51 +0900

    ユーザープロフィールのコントローラ等を作成

---
+++ app/controllers/users/profiles_controller.rb
@@ -0,0 +1,37 @@
+class Users::ProfilesController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: t("user.update")
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :postcode, :address, :biography)
+    end
+
+    def correct_user
+      @user = set_user
+      redirect_to users_path unless @user == current_user
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: "books#index"
 
-  scope path: ":locale" do
-    resources :books
+  scope module: :users do
+    resources :users, only: [:index, :show, :edit, :update], controller: :profiles
   end
+  resources :books
+  
 end
