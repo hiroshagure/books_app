55e9923e993fcd5ab9eab6a3db21eba15b2badda
hiroshagure <hiroshagure@gmail.com>
2019-12-05 15:26:04 +0900

    不要なコードを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,4 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       current_user
     end
-
-  # def after_sign_out_path_for(resource)
-  # end
 end
