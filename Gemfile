53a2f46579b3db61de899fc311460b750a449e3a
hiroshagure <hiroshagure@gmail.com>
2019-12-19 18:49:46 +0900

    OmniAuthを導入

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem "carrierwave"
 gem "kaminari"
 gem "devise"
 gem "devise-i18n"
+gem "omniauth"
+gem "omniauth-github"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -40,6 +42,7 @@ gem "bootsnap", ">= 1.4.2", require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
+  gem "dotenv-rails"
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_011759) do
+ActiveRecord::Schema.define(version: 2019_11_29_012836) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_011759) do
     t.integer "postcode"
     t.string "address"
     t.text "biography"
+    t.string "provider", default: "", null: false
+    t.text "uid", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
